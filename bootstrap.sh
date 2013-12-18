5a640b483c729d69ded543cfd93233cba9389a2c
danilo <danilo@danilo-PC>
2013-12-18 10:32:41 +0100

    don't support mingw

--- bootstrap.sh
+++ bootstrap.sh
@@ -12,19 +12,20 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+# //FIXME: error checking, proper on configure or half downloaded archives
 set -u
 set -e
 
 DIR=`pwd`
-PATHO=$PATH
-UNAME=`uname -s | grep -m1 -ioP '([a-z])+' | awk 'NR==1{print $0}'` # proper detection of OS
+UNAME=`uname -s | grep -m1 -ioE '[a-z]+' | awk 'NR==1{print $0}'` # detection of OS
 CROSSDIR="$DIR/cc/$UNAME" # crosstools dir
 
 KEEP=0 # keep stuff or not
+WIN=0
 BUILDARCHS="x86_64-pc-elf" # x86_64-pc-elf, "x86_64-pc-elf i686-pc-elf aarch64-none-elf"
 BUILDBACKENDS="gdc" # gdc, "gdc ldc dmd"
 
-while getopts "a:ckb:v" opt; do
+while getopts "ckva:b:" opt; do
 	case "$opt" in
 		a) # select architectures to build
 			BUILDARCHS=${OPTARG,,}
@@ -36,9 +37,11 @@ while getopts "a:ckb:v" opt; do
 			rm -rf $CROSSDIR/binutils-*
 			rm -rf $CROSSDIR/gcc-*
 			rm -rf $CROSSDIR/gdc
+			rm -rf $CROSSDIR/gdb-*
 			rm -rf $CROSSDIR/ldc
 			rm -rf $CROSSDIR/bochs-*
 			rm -rf $CROSSDIR/mtools-*
+			rm -rf $CROSSDIR/llvm*
 			rm -rf $CROSSDIR/dmd
 			exit 0
 		;;
@@ -51,8 +54,19 @@ while getopts "a:ckb:v" opt; do
 	esac
 done
 
-# simple checks
-TOOLS="curl git bison flex make gcc texindex gdb nasm tar xzcat python patch"
+if [[ "$UNAME" == "CYGWIN" || "$UNAME" == "MINGW" ]]; then
+	WIN=1
+fi
+
+
+# mingw will fail
+if [ "$UNAME" == "MINGW" ]; then
+	echo "mingw not supported; exiting"
+	exit 0;
+fi
+
+# check for tools
+TOOLS="curl git svn bison flex make gcc gdb texindex tar xzcat python patch"
 for TOOL in $TOOLS; do
 	if ! which "$TOOL"; then
 		echo "$TOOL not found; exiting"
@@ -60,19 +74,28 @@ for TOOL in $TOOLS; do
 	fi
 done
 
+if [[ $BUILDBACKENDS =~ "ldc" ]]; then
+	if ! which "cmake"; then
+		echo "cmake not found; exiting"
+		exit 0;
+	fi
+fi
+
+
 
 if [ ! -f "$CROSSDIR/bin/" ]; then
 	mkdir -p "$CROSSDIR"
 	mkdir -p "$CROSSDIR/bin/"
 fi
 
-# check for cross compile tools
+
+# build cross compile tools
 for BUILDARCH in $BUILDARCHS; do
 	LD="$CROSSDIR/bin/$BUILDARCH-ld"
 	GCC="$CROSSDIR/bin/$BUILDARCH-gcc"
 	LDC="$CROSSDIR/bin/ldc2"
-	BOCHS="$CROSSDIR/bin/bochs"
 	DMD="$CROSSDIR/bin/dmd"
+	BOCHS="$CROSSDIR/bin/bochs"
 
 	if [ ! -f "$LD" ]; then
 		BINSRCDIR="$CROSSDIR/binutils-2.24"
@@ -83,7 +106,6 @@ for BUILDARCH in $BUILDARCHS; do
 		if [ ! -d "$BINSRCDIR" ]; then
 			test -f "$BINARCHIVE" || curl -o "$BINARCHIVE" "http://ftp.gnu.org/gnu/binutils/binutils-2.24.tar.bz2"
 			tar -xjf "$BINARCHIVE" -C "$CROSSDIR"
-			rm -rf "$BINARCHIVE"
 		fi
 
 		mkdir -p "$BINBUILD"
@@ -91,13 +113,13 @@ for BUILDARCH in $BUILDARCHS; do
 		export TARGET="$BUILDARCH"
 
 		cd "$BINBUILD"
-		#make clean
-		../binutils-2.24/configure --target="$BUILDARCH" --prefix="$CROSSDIR" --disable-nls --enable-64-bit-bfd # can we do this here?
+		../binutils-2.24/configure --target="$BUILDARCH" --prefix="$CROSSDIR" --disable-nls --enable-64-bit-bfd
+
 		make all
 		make install
 	fi
 
-	if [[ ! -f "$GCC" && $BUILDBACKENDS =~ "gdc" ]]; then
+	if [ ! -f "$GCC" ]; then
 		GCCSRCDIR="$CROSSDIR/gcc-4.8.2"
 		GCCARCHIVE="$CROSSDIR/gcc-4.8.2.tar.bz2"
 		GCCBUILD="$CROSSDIR/gcc-4.8.2-$BUILDARCH"
@@ -106,7 +128,6 @@ for BUILDARCH in $BUILDARCHS; do
 		if [ ! -d "$GCCSRCDIR" ]; then
 			test -f "$GCCARCHIVE" || curl -o "$GCCARCHIVE" "ftp://ftp.gnu.org/gnu/gcc/gcc-4.8.2/gcc-4.8.2.tar.bz2"
 			tar -xjf "$GCCARCHIVE" -C "$CROSSDIR"
-			rm -rf "$GCCARCHIVE"
 		fi
 
 		# fetch iconv
@@ -142,7 +163,7 @@ for BUILDARCH in $BUILDARCHS; do
 		fi
 
 		# fetch gdc
-		if [ ! -d "$CROSSDIR/gdc/dev" ]; then
+		if [[ ! -d "$CROSSDIR/gdc/dev" && $BUILDBACKENDS =~ "gdc" ]]; then
 			mkdir -p "$CROSSDIR/gdc"
 			cd "$CROSSDIR"
 			git clone https://github.com/D-Programming-GDC/GDC.git "$CROSSDIR/gdc/dev"
@@ -155,40 +176,58 @@ for BUILDARCH in $BUILDARCHS; do
 		mkdir -p "$GCCBUILD"
 		export PREFIX="$CROSSDIR"
 		export TARGET="$BUILDARCH"
-		export PATH="$CROSSDIR/bin:$PATHO:$CROSSDIR/bin"
 
 		cd "$GCCBUILD"
-		#make clean
 		../gcc-4.8.2/configure --target="$BUILDARCH" --prefix="$CROSSDIR" --disable-nls --enable-languages=c,c++,d --without-headers --disable-libphobos
+
 		make all-gcc
 		make install-gcc
 	fi
 
 
-	if [[ ! -f "$LDC" && $BUILDBACKENDS =~ "ldc" && "$UNAME" != "CYGWIN" ]]; then
+	if [[ ! -f "$LDC" && $BUILDBACKENDS =~ "ldc" && $WIN -eq 0 ]]; then
+		cd "$CROSSDIR"
+
+		# build llvm/clang first if needed
+		if ! which "clang"; then
+			cd "$CROSSDIR"
+			test -d "$CROSSDIR/llvm" || svn co http://llvm.org/svn/llvm-project/llvm/trunk llvm
+			cd "$CROSSDIR/llvm/tools"
+			test -d "$CROSSDIR/llvm/tools/clang" || svn co http://llvm.org/svn/llvm-project/cfe/trunk clang
+			cd "$CROSSDIR/llvm/tools/clang/tools"
+			test -d "$CROSSDIR/llvm/tools/clang/tools/extra" || svn co http://llvm.org/svn/llvm-project/clang-tools-extra/trunk extra
+			cd "$CROSSDIR/llvm/projects" 
+			test -d "$CROSSDIR/llvm/projects/compiler-rt" || svn co http://llvm.org/svn/llvm-project/compiler-rt/trunk compiler-rt
+
+			mkdir -p "$CROSSDIR/llvm-build"
+			../llvm/configure --enable-optimized
+
+			make
+			make install
+
+			cd "$CROSSDIR"
+		fi
+
 		LDCBUILD="$CROSSDIR/ldc/build-$BUILDARCH"
 
-		cd "$CROSSDIR"
 		test -d "$CROSSDIR/ldc" || git clone --recursive https://github.com/ldc-developers/ldc.git
 
 		mkdir -p "$LDCBUILD"
 		export PREFIX="$CROSSDIR"
 		export TARGET="$BUILDARCH"
-		export PATH="$CROSSDIR/bin:$PATHO:$CROSSDIR/bin"
+
 		cd "$LDCBUILD"
 
-		#make clean 
 		cmake .. -DCMAKE_INSTALL_PREFIX="$CROSSDIR" -DINCLUDE_INSTALL_DIR="$CROSSDIR/include"
 		make
 		make install
 	fi
 
 
-	if [[ ! -f "$DMD" && $BUILDBACKENDS =~ "dmd" && "$UNAME" != "CYGWIN" ]]; then
+	if [[ ! -f "$DMD" && $BUILDBACKENDS =~ "dmd" && $WIN -eq 0 ]]; then
 		cd "$CROSSDIR"
 		test -d "$CROSSDIR/dmd" || git clone --recursive https://github.com/D-Programming-Language/dmd.git
 
-		#make clean 
 		cd "$CROSSDIR/dmd"
 		make -f posix.mak MODEL=64 TARGET_CPU=X86
 		cp src/dmd "$CROSSDIR/bin/dmd"
@@ -200,11 +239,13 @@ for BUILDARCH in $BUILDARCHS; do
 		cd "$CROSSDIR"
 		test -f "$CROSSDIR/bochs-2.6.2.tar.gz" || curl -o "$CROSSDIR/bochs-2.6.2.tar.gz" -L http://downloads.sourceforge.net/project/bochs/bochs/2.6.2/bochs-2.6.2.tar.gz
 
-		test -d "$CROSSDIR/bochs-2.6.2" || tar -xzf "$CROSSDIR/bochs-2.6.2.tar.gz" -C "$CROSSDIR"
+		if [ ! -d "$CROSSDIR/bochs-2.6.2" ]; then
+			tar -xzf "$CROSSDIR/bochs-2.6.2.tar.gz" -C "$CROSSDIR"
+			cd "$CROSSDIR/bochs-2.6.2"
+			patch -p1 < ../../../support/bochs.patch
+		fi
 
-		#make clean
 		cd "$CROSSDIR/bochs-2.6.2"
-		patch -p1 < ../../../support/bochs.patch
 		./configure --disable-plugins --enable-x86-64 --enable-smp --enable-cpu-level=6 --enable-large-ramfile --enable-ne2000 --enable-pci --enable-usb --enable-usb-ohci --enable-e1000 --enable-debugger --enable-disasm --enable-debugger-gui --enable-iodebug --enable-all-optimizations --enable-logging --enable-fpu --enable-vmx --enable-svm --enable-avx --enable-x86-debugger --enable-cdrom --enable-sb16=dummy --disable-docbook --with-x --with-x11 --with-term --prefix="$CROSSDIR"
 
 		make
@@ -213,33 +254,36 @@ for BUILDARCH in $BUILDARCHS; do
 done
 
 MTOOLS="$CROSSDIR/bin/mtools"
-if [[ ! -f "$MTOOLS" && "$UNAME" == "CYGWIN" ]]; then
+if [[ ! -f "$MTOOLS"  && $WIN -eq 1 ]]; then
 	test -f "$CROSSDIR/mtools-4.0.18.tar.bz2" || curl -o "$CROSSDIR/mtools-4.0.18.tar.bz2" ftp://ftp.gnu.org/gnu/mtools/mtools-4.0.18.tar.bz2
-	test -d "$CROSSDIR/mtools-4.0.18" || tar -xjf "$CROSSDIR/mtools-4.0.18.tar.bz2" -C "$CROSSDIR"
 
-	#make clean
+	if [ ! -d "$CROSSDIR/mtools-4.0.18" ]; then
+		tar -xjf "$CROSSDIR/mtools-4.0.18.tar.bz2" -C "$CROSSDIR"
+		cd "$CROSSDIR/mtools-4.0.18"
+		patch -p1 < ../../../support/mtools.patch
+	fi
+
 	cd "$CROSSDIR/mtools-4.0.18"
-	patch -p1 < ../../../support/mtools.patch
 
 	export PREFIX="$CROSSDIR"
 	export TARGET="$BUILDARCH"
-	export PATH="$CROSSDIR/bin:$PATHO:$CROSSDIR/bin"
 
 	./configure --prefix="$CROSSDIR"
 
 	make
 	make install
-
-	echo "drive z: file=\"$DIR/build/kernel.img\" partition=1" > ~/.mtoolsrc
 fi
+
 
 if [ $KEEP -eq 0 ]; then
 	rm -rf $CROSSDIR/binutils-*
 	rm -rf $CROSSDIR/gcc-*
 	rm -rf $CROSSDIR/gdc
+	rm -rf $CROSSDIR/gdb-*
 	rm -rf $CROSSDIR/ldc
 	rm -rf $CROSSDIR/bochs-*
 	rm -rf $CROSSDIR/mtools-*
+	rm -rf $CROSSDIR/llvm*
 	rm -rf $CROSSDIR/dmd
 fi
 
@@ -251,8 +295,3 @@ if [ ! -f "waf" ]; then
 	curl -o "$DIR/waf" "http://waf.googlecode.com/files/waf-1.7.13"
 	chmod a+rx "$DIR/waf"
 fi
-
-EXTRAS="build_logs eclipse objcopy relocation syms"
-for EXTRA in $EXTRAS; do
-	./waf update --files="$EXTRA"
-done
